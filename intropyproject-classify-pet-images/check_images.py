eadd279a73a1664d8f13e3058dbb52aecf63a42f
George Zaldivar <zetabasq001@outlook.com>
2019-05-25 19:24:20 -0400

    changed None parameter to command line arg parameter

--- intropyproject-classify-pet-images/check_images.py
+++ intropyproject-classify-pet-images/check_images.py
@@ -81,40 +81,40 @@ def main():
     check_classifying_images(results)
 
 
-    # # TODO 4: Define adjust_results4_isadog function within the file adjust_results4_isadog.py
-    # # Once the adjust_results4_isadog function has been defined replace 'None'
-    # # in the function call with in_arg.dogfile  Once you have done the
-    # # replacements your function call should look like this:
-    # #          adjust_results4_isadog(results, in_arg.dogfile)
-    # # Adjusts the results dictionary to determine if classifier correctly
-    # # classified images as 'a dog' or 'not a dog'. This demonstrates if
-    # # model can correctly classify dog images as dogs (regardless of breed)
-    # adjust_results4_isadog(results, None)
-    #
-    # # Function that checks Results Dictionary for is-a-dog adjustment using results
-    # check_classifying_labels_as_dogs(results)
-    #
-    #
-    # # TODO 5: Define calculates_results_stats function within the file calculates_results_stats.py
-    # # This function creates the results statistics dictionary that contains a
-    # # summary of the results statistics (this includes counts & percentages). This
-    # # dictionary is returned from the function call as the variable results_stats
-    # # Calculates results of run and puts statistics in the Results Statistics
-    # # Dictionary - called results_stats
-    # results_stats = calculates_results_stats(results)
-    #
-    # # Function that checks Results Statistics Dictionary using results_stats
-    # check_calculating_results(results, results_stats)
-    #
-    #
-    # # TODO 6: Define print_results function within the file print_results.py
-    # # Once the print_results function has been defined replace 'None'
-    # # in the function call with in_arg.arch  Once you have done the
-    # # replacements your function call should look like this:
-    # #      print_results(results, results_stats, in_arg.arch, True, True)
-    # # Prints summary results, incorrect classifications of dogs (if requested)
-    # # and incorrectly classified breeds (if requested)
-    # print_results(results, results_stats, None, True, True)
+    # TODO 4: Define adjust_results4_isadog function within the file adjust_results4_isadog.py
+    # Once the adjust_results4_isadog function has been defined replace 'None'
+    # in the function call with in_arg.dogfile  Once you have done the
+    # replacements your function call should look like this:
+    #          adjust_results4_isadog(results, in_arg.dogfile)
+    # Adjusts the results dictionary to determine if classifier correctly
+    # classified images as 'a dog' or 'not a dog'. This demonstrates if
+    # model can correctly classify dog images as dogs (regardless of breed)
+    adjust_results4_isadog(results, in_arg.dogfile)
+
+    # Function that checks Results Dictionary for is-a-dog adjustment using results
+    check_classifying_labels_as_dogs(results)
+
+
+    # TODO 5: Define calculates_results_stats function within the file calculates_results_stats.py
+    # This function creates the results statistics dictionary that contains a
+    # summary of the results statistics (this includes counts & percentages). This
+    # dictionary is returned from the function call as the variable results_stats
+    # Calculates results of run and puts statistics in the Results Statistics
+    # Dictionary - called results_stats
+    results_stats = calculates_results_stats(results)
+
+    # Function that checks Results Statistics Dictionary using results_stats
+    check_calculating_results(results, results_stats)
+
+
+    # TODO 6: Define print_results function within the file print_results.py
+    # Once the print_results function has been defined replace 'None'
+    # in the function call with in_arg.arch  Once you have done the
+    # replacements your function call should look like this:
+    #      print_results(results, results_stats, in_arg.arch, True, True)
+    # Prints summary results, incorrect classifications of dogs (if requested)
+    # and incorrectly classified breeds (if requested)
+    print_results(results, results_stats, None, True, True)
 
     # TODO 0: Measure total program runtime by collecting end time
     end_time = time()
