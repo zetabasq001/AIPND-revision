a599cc8e681dfc25969a3159f5ed54d57c7a1f0d
George Zaldivar <zetabasq001@outlook.com>
2019-05-25 15:42:01 -0400

    Defined function get_pet_labels

--- intropyproject-classify-pet-images/get_pet_labels.py
+++ intropyproject-classify-pet-images/get_pet_labels.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
-#                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
-# PURPOSE: Create the function get_pet_labels that creates the pet labels from 
-#          the image's filename. This function inputs: 
-#           - The Image Folder as image_dir within get_pet_labels function and 
-#             as in_arg.dir for the function call within the main function. 
+#
+# PROGRAMMER:
+# DATE CREATED:
+# REVISED DATE:
+# PURPOSE: Create the function get_pet_labels that creates the pet labels from
+#          the image's filename. This function inputs:
+#           - The Image Folder as image_dir within get_pet_labels function and
+#             as in_arg.dir for the function call within the main function.
 #          This function creates and returns the results dictionary as results_dic
-#          within get_pet_labels function and as results within main. 
+#          within get_pet_labels function and as results within main.
 #          The results_dic dictionary has a 'key' that's the image filename and
 #          a 'value' that's a list. This list will contain the following item
 #          at index 0 : pet image label (string).
@@ -20,14 +20,14 @@
 from os import listdir
 
 # TODO 2: Define get_pet_labels function below please be certain to replace None
-#       in the return statement with results_dic dictionary that you create 
+#       in the return statement with results_dic dictionary that you create
 #       with this function
-# 
+#
 def get_pet_labels(image_dir):
     """
-    Creates a dictionary of pet labels (results_dic) based upon the filenames 
-    of the image files. These pet image labels are used to check the accuracy 
-    of the labels that are returned by the classifier function, since the 
+    Creates a dictionary of pet labels (results_dic) based upon the filenames
+    of the image files. These pet image labels are used to check the accuracy
+    of the labels that are returned by the classifier function, since the
     filenames of the images contain the true identity of the pet in the image.
     Be sure to format the pet labels so that they are in all lower case letters
     and with leading and trailing whitespace characters stripped from them.
@@ -36,10 +36,44 @@ def get_pet_labels(image_dir):
      image_dir - The (full) path to the folder of images that are to be
                  classified by the classifier function (string)
     Returns:
-      results_dic - Dictionary with 'key' as image filename and 'value' as a 
+      results_dic - Dictionary with 'key' as image filename and 'value' as a
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
+    # Retrieve the filenames from folder pet_images/
+    filenames_list = listdir("./pet_images/")
+
+    # Creates empty dictionary named results_dic
+    results_dic = dict()
+
+    # iterate through filenames
+    for filename in filenames_list:
+
+        # Sets pet_image variable to a filename
+        pet_image = filename
+
+        # Sets string to lower case letters
+        low_pet_image = pet_image.lower()
+
+        # Splits lower case string by _ to break into words
+        word_list_pet_image = low_pet_image.split("_")
+
+        # Create pet_name starting as empty string
+        pet_name = ""
+
+        # Loops to check if word in pet name is only
+        # alphabetic characters - if true append word
+        # to pet_name separated by trailing space
+        for word in word_list_pet_image:
+            if word.isalpha():
+                pet_name += word + " "
+
+        # Strip off starting/trailing whitespace characters
+        pet_name = pet_name.strip()
+
+        # Set filename key to pet_name in list value
+        results_dic[filename] = [pet_name]
+
     # Replace None with the results_dic dictionary that you created with this
     # function
-    return None
+    return results_dic
